fef0245eda9aec11d89b2c72ec327151be920004
Quang <quankle2004@gmail.com>
2025-07-11 11:04:08 +0700

    Initial push to shophoa-backend

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/products/category/{category}', [ProductController::class, 'getByCategory']);
 Route::get('/products/category/{category}/{style?}', [ProductController::class, 'getByCategoryAndStyle']);
 Route::get('/products/top', [ProductController::class, 'getByIsOnTop']);
 Route::resource('users', UserController::class);
 Route::apiResource('products', ProductController::class);
+Route::get('/debug-db', function () {
+    return DB::select('SELECT DATABASE() as db_name');
+});
+Route::get('/debug-products', function () {
+    return DB::table('products')->get();
+});
